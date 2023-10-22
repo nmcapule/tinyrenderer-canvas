df08bd1683f808e862b1293a924a199aaabb5613
Nathaniel M. Capule <llss.s.sls.lll@gmail.com>
2023-10-22 15:40:30 +0000

    feat: Some buggy way to load a model :P

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,33 @@
-import { CanvasWrapper } from "./canvas_wrapper";
-import {
-  drawTestFilledTriangles,
-  drawTestRandomFilledTriangles,
-} from "./examples";
+import { CanvasWrapper, Point, Color } from "./canvas_wrapper";
+import { drawTestRandomFilledTriangles } from "./examples";
 
-function render() {
+async function loadModel(url: string) {
+  const response = await fetch(new URL(url));
+  const payload = await response.text();
+
+  const object = {
+    v: [],
+    vt: [],
+    vn: [],
+    f: [],
+  };
+
+  for (const line of payload.split("\n")) {
+    const parts = line.split(" ");
+    if (!["v", "vt", "vn", "f"].includes(parts[0])) {
+      continue;
+    }
+    const t = parts[0];
+    const x = parseFloat(parts[1]);
+    const y = parseFloat(parts[2]);
+    const z = parseFloat(parts[3]);
+    object[t].push([x, y, z]);
+  }
+
+  return object;
+}
+
+async function render() {
   const canvas = document.getElementById("canvas") as HTMLCanvasElement;
   if (!canvas.getContext) {
     throw new Error("Canvas not supported");
@@ -14,9 +37,30 @@ function render() {
   w.useContextAPI = true;
   w.clear();
 
+  const model = await loadModel(`${window.location.href}african_head.obj`);
+
   const start = performance.now();
-  // drawTestFilledTriangles(w);
-  drawTestRandomFilledTriangles(w, 100);
+  for (let i = 0; i < model["f"].length; i++) {
+    const face = model["f"][i];
+    for (let j = 0; j < 3; j++) {
+      const v0 = model["v"][face[j]];
+      const v1 = model["v"][face[(j + 1) % 3]];
+      try {
+        const x0 = ((v0[0] + 1) * w.canvas.width) / 2;
+        const y0 = ((v0[1] + 1) * w.canvas.height) / 2;
+        const x1 = ((v1[0] + 1) * w.canvas.width) / 2;
+        const y1 = ((v1[1] + 1) * w.canvas.height) / 2;
+        w.drawLine(
+          new Point(x0, canvas.height - y0),
+          new Point(x1, canvas.height - y1),
+          new Color(0, 0, 0, 255)
+        );
+      } catch (e) {
+        console.log(v0, v1);
+        console.error(e);
+      }
+    }
+  }
   const end = performance.now();
   console.log(`Took ${end - start}ms`);
 }
